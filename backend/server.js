3ec9d60ed3c99fa21dd800afc8d49cade54dc5b5
Amkam <amaduskamara@gmail.com>
2022-07-16 11:32:54 +0000

    Configure express app and startup the server with a single route

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
-console.log('Server');
+const express = require("express");
+const dotenv = require("dotenv").config();
+const cors = require("cors");
+
+const PORT = process.env.PORT || 5000;
+
+// Express app
+const app = express();
+
+// Routes
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Welcome to the support desk API" });
+});
+
+// Start listening
+app.listen(PORT, () => console.log(`Server started on ${PORT}`));
