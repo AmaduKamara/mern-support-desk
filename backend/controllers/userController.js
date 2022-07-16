8f4e4474c7113110cd64248c584db0f30f8fef76
Amkam <amaduskamara@gmail.com>
2022-07-16 12:21:31 +0000

    Add custom error handler and fetch req body for registering a user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,25 @@
-const registerUser = (req, res) => {
-  res.send("Register User Route");
-};
+const asyncHandler = require("express-async-handler");
 
-const loginUser = (req, res) => {
+// @description     Register a User
+// @route           /api/users
+// @access          Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  // Validation
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please include all fields");
+  }
+
+  res.send("Register User Route");
+});
+
+// @description     Login a User
+// @route           /api/users/login
+// @access          Public
+const loginUser = asyncHandler(async (req, res) => {
   res.send("Login User Route");
-};
+});
 
 module.exports = { registerUser, loginUser };
--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,28 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
+const colors = require("colors")
 
+const { errorHandler } = require("./middleware/errorMiddleware");
 
 const PORT = process.env.PORT || 5000;
 
 // Express app
 const app = express();
 
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+
+
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Welcome to the support desk API" });
 });
 
 // Routes
-app.use('/api/users', require('./routes/userRoutes'))
+app.use("/api/users", require("./routes/userRoutes"));
 
+app.use(errorHandler);
 
 // Start listening
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
