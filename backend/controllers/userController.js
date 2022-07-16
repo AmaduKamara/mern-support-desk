687f9bf029de7a7a4ba62f6526fcf0ace0929b48
Amkam <amaduskamara@gmail.com>
2022-07-16 13:08:10 +0000

    Add user creation logic

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,13 @@
 const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcryptjs");
+
+const User = require("../models/userModel");
 
 // @description     Register a User
 // @route           /api/users
 // @access          Public
 const registerUser = asyncHandler(async (req, res) => {
+  // Getting input fields data as body data
   const { name, email, password } = req.body;
 
   // Validation
@@ -12,7 +16,32 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new Error("Please include all fields");
   }
 
-  res.send("Register User Route");
+  // Find if user already exist
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  // Hash user password that gets saved to the database
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // Create user
+  const user = await User.create({ name, email, password: hashedPassword });
+
+  // Return the created user
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
 });
 
 // @description     Login a User
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -5,19 +5,23 @@ const userSchema = mongoose.Schema(
     name: {
       type: String,
       required: [true, "Please add a name"],
+      trim: true,
     },
     email: {
       type: String,
       required: [true, "Please add an email"],
+      trim: true,
     },
     password: {
       type: String,
       required: [true, "Please add a password"],
+      trim: true,
     },
     isAdmin: {
       type: Boolean,
       required: true,
       default: false,
+      trim: true,
     },
   },
   {
