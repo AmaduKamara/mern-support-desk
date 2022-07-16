e6c20f9c07271428f98cea843ec0e006cf7add49
Amkam <amaduskamara@gmail.com>
2022-07-16 13:45:04 +0000

    Add getMe route for getting user data

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -70,8 +70,15 @@ const loginUser = asyncHandler(async (req, res) => {
   }
 });
 
+// @description     Get current User
+// @route           /api/users/me
+// @access          Private
+const getMe = asyncHandler(async (req, res) => {
+  res.send("Me");
+});
+
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "7d" });
 };
 
-module.exports = { registerUser, loginUser };
+module.exports = { registerUser, loginUser, getMe };
