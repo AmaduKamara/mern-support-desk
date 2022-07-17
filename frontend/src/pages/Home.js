2e21736772d95dbedfeac43201337b14f6460787
Amkam <amaduskamara@gmail.com>
2022-07-17 03:00:35 +0000

    Add Home page content options

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,21 @@
+import { Link } from "react-router-dom";
+import { FaTicketAlt, FaQuestionCircle } from "react-icons/fa";
+
 function Home() {
-  return <h1>Home</h1>;
+  return (
+    <>
+      <section className="heading">
+        <h1>What do your need help with</h1>
+        <p>Please choose from an option below.</p>
+        <Link to="/new-ticket" className="btn btn-block">
+          <FaQuestionCircle /> Create New Ticket
+        </Link>
+        <Link to="/tickets" className="btn btn-reverse btn-block">
+          <FaTicketAlt /> View My Tickets
+        </Link>
+      </section>
+    </>
+  );
 }
 
 export default Home;
